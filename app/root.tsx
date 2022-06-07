5edd4a1e9b03922f9a17497002f963db9cf998ea
Rio Tolinggi <lovelytolinggi@gmail.com>
2022-06-08 00:43:17 +0800

    install mantine css

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { NotificationsProvider } from "@mantine/notifications";
+import { MantineProvider } from "@mantine/core";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -14,7 +16,7 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export default function App() {
+const Document = () => {
   return (
     <html lang="en">
       <head>
@@ -28,5 +30,15 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
+  );
+};
+
+export default function App() {
+  return (
+    <MantineProvider>
+      <NotificationsProvider position="top-right">
+        <Document />
+      </NotificationsProvider>
+    </MantineProvider>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,23 @@
-export default function Index() {
+import { Container, Center, Button } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
+
+type Message = {
+  title: string;
+  message: string;
+  color?: string;
+};
+
+export default function IndexRoute() {
+  const message: Message = {
+    title: "Registrasi Berhasil",
+    message: "Untuk mengaktifkan account, silahkan cek email masuk..!!",
+    color: "cyan",
+  };
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Container size="md" p="sm">
+      <Center>
+        <Button onClick={() => showNotification(message)}>Notifikasi</Button>
+      </Center>
+    </Container>
   );
 }
